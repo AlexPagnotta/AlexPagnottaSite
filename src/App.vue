c4d5d6ce01b942637a4f883d438d207f9f84c6ce
Alex <alex.pagnotta@outlook.it>
2020-11-03 07:52:18 +0100

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
       <NavBar/>
       <Header title="Ciao sono Alex Pagnotta e questo è il mio sito"/>
       <Divider/>
+      <Divider/>
+      <Footer 
+      imageSrc= "https://bulma.io/images/placeholders/128x128.png"
+      text ="Sono Alex Pagnotta, ho 22 anni, e sin da piccolo ho sempre avuto una grande passione per tutto quello che riguarda l'informatica e la programmazione, passione che da qualche anno è diventato il mio lavoro.
+      
+            In questi anni ho principalmente lavorato in ambiente .Net, utilizzando .Net Core e approfondendo Azure e il cloud in generale, mentre nel tempo libero mi sono interessato anche allo sviluppo Frontend e Mobile, utilizzando varie tecnologie, tra cui React e React Native.
+      "/>
   </div>
 </template>
 
@@ -11,11 +18,12 @@
 import NavBar from './components/NavBar.vue'
 import Header from './components/Header.vue'
 import Divider from './components/Divider.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar, Header, Divider
+    NavBar, Header, Divider, Footer
   }
 }
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,73 @@
+<template> 
+  <section class="section is-flex is-justify-content-center">
+    <div class="Footer__box box">
+      <article class="Footer__article media">
+        <div class="Footer__image media-left">
+          <figure class=" image is-96x96">
+            <img :src="imageSrc" alt="Image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <p class="Footer__text">
+              {{text}}
+            </p>
+          </div>
+        </div>
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+  props: {
+    imageSrc: String,
+    text: String
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+@import "../assets/sass/main.scss";
+
+@include mobile {
+
+    .Footer__article{
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .Footer__image{
+      padding-right: 0px;
+      padding-bottom: 2rem;
+    }
+}
+
+
+@include tablet {
+
+    .Footer__box{
+      width: 800px;
+    }
+
+    .Footer__image{
+      padding-right: 1.5rem;
+    }
+}
+
+.Footer__image img {
+  border-radius: 100px;
+  border: 5px solid $white;
+}
+
+.Footer__text{
+  white-space: pre-line;
+}
+
+</style>
